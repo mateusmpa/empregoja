9c792881d28170f7b891a73021bacab8d71e9f63
Campus Code <time@campuscode.com>
2015-09-14 22:18:04 -0300

    user add company

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,22 @@
+class CompaniesController < ApplicationController
+  def new
+    @company = Company.new
+  end
+  def create
+    @company = Company.new(company_params)
+    if(@company.save)
+      redirect_to @company
+    else
+      render 'new'
+    end
+  end
+  def show
+    @company = Company.find(params[:id])
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :location, :mail, :phone)
+  end
+end
